f63f56a717f1758f696668264a998dba95d38f13
ACurguz <aleksandracurguz@gmail.com>
2017-08-07 11:42:38 +0200

    added spring-boot-starter-web

---
+++ src/main/java/rs/levi9/tech9/team3/OnlineVideoOrganizerApplication.java
@@ -0,0 +1,13 @@
+package rs.levi9.tech9.team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVideoOrganizerApplication {
+	
+	public static void main(String[] args) {
+		 SpringApplication.run(OnlineVideoOrganizerApplication.class, args);
+	}
+
+}
