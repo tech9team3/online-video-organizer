768b813e5db118fec0831e27fc3e2eafb15546ea
Dejan Ivanovic <ivanovic.dejan@gmail.com>
2017-08-10 16:06:04 +0200

    frontend (starting point)

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,12 @@
+(function () {
+    var app = angular.module('app', ['ngRoute', 'ngResource', 'ui.bootstrap', 'vcRecaptcha']);
+
+    // If we implement the basic security in spring boot then the response will
+    // contains the header 'WWW-Authenticate: Basic'. So the browser will popup a
+    // alert to get the user credentials. To avoid that we have to set a header in
+    // every request we are making using AngularJs.
+    app.config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+    }]);
+})();
+
