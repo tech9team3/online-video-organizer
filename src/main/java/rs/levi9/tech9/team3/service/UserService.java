0ce7a5bd8dba5cb43b44aacca3df308db5cdfb33
VladimirJevremov <jewraaaa@gmail.com>
2017-08-16 19:48:02 +0200

    email notification when registering

--- src/main/java/rs/levi9/tech9/team3/service/UserService.java
+++ src/main/java/rs/levi9/tech9/team3/service/UserService.java
@@ -2,7 +2,10 @@ package rs.levi9.tech9.team3.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.stereotype.Service;
 
 import rs.levi9.tech9.team3.domain.User;
@@ -14,11 +17,15 @@ public class UserService {
 
 	private UserRepository userRepository;
 	private VideoListService videoListService;
+	private NotificationService notificationService;
+	private Logger logger = LoggerFactory.getLogger(UserService.class);
 
 	@Autowired
-	public UserService(UserRepository userRepository,VideoListService videoListService) {
+	public UserService(UserRepository userRepository, VideoListService videoListService,
+			NotificationService notificationService) {
 		this.userRepository = userRepository;
 		this.videoListService = videoListService;
+		this.notificationService = notificationService;
 	}
 
 	public List<User> findAll() {
@@ -30,6 +37,9 @@ public class UserService {
 	}
 
 	public User save(User user) {
+		if(user.getId()==null) {
+			notificationService.sendRegistrationNotification(user);
+		}
 		return userRepository.save(user);
 	}
 
@@ -41,11 +51,12 @@ public class UserService {
 		}
 		userRepository.delete(id);
 	}
-	
+
 	public User findOneByEmail(String email) {
 		return userRepository.findByEmail(email);
 	}
-	public User findOneByUsername (String username) {
+
+	public User findOneByUsername(String username) {
 		return userRepository.findByUsername(username);
 	}
 }
