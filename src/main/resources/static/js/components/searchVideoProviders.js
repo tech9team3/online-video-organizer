db82e6bdbdd95fbdc6cc05e1bde3786291626bb7
Dejan Ivanović <ivanovic.dejan@gmail.com>
2017-08-20 18:24:41 +0200

    search video providers (not completed)

--- src/main/resources/static/js/components/searchVideoProviders.js
+++ src/main/resources/static/js/components/searchVideoProviders.js
@@ -5,11 +5,15 @@
             bindings: {
 
             },
-            controller: function ($http,$cookieStore) {
+            controller: function ($http, $cookieStore) {
                 var ctrl = this;
+
                 ctrl.searchYouTube = searchYouTube;
                 ctrl.searchVimeo = searchVimeo;
                 ctrl.searchDailymotion = searchDailymotion;
+
+                ctrl.providerVideoList = [];
+                ctrl.progressBar = false;
 
                 //YouTube
                 const YOUTUBE_SERARCH_API_URL = "https://www.googleapis.com/youtube/v3/search";
@@ -23,12 +27,14 @@
                 const VIMEO_AUTHORIZE_URL = "https://api.vimeo.com/oauth/authorize/client";
                 const VIMEO_ACCESS_TOKEN_URL = "https://api.vimeo.com/oauth/access_token";
                 const VIMEO_RETURN_FIELDS = "uri,name,link,description,tags.name,pictures.sizes.link";
+                const VIMEO_TOKEN = "24649bd87ad57cf0331a1a5754464bf1";
 
                 //Dailymotion
                 const DAILYMOTION_SERARCH_API_URL = "https://api.dailymotion.com/videos";
                 const DAILYMOTION_RETURN_FIELDS = "description,id,tags,thumbnail_360_url,title";
 
                 //var vimeoToken = generateVimeoToken();
+                var vimeoToken = VIMEO_TOKEN;
 
                 function generateVimeoToken() {
                     var base64Credential = btoa(VIMEO_CLIENT_IDENTIFIER + ':' + VIMEO_CLIENT_SECRET);
@@ -46,14 +52,22 @@
 
                     return $http(req)
                         .then(function (response) {
-                            $cookieStore.put('vimeoToken', response.data.access_token);
+                            //$cookieStore.put('vimeoToken', response.data.access_token);
+                            console.log(response.data.access_token);
                             return response.data.access_token;
+                        }, function () {
+                            // Error handler
+                        }).finally(function () {
+                            // Always execute this on both error and success
                         });
                 }
 
 
                 //Search YouTube
                 function searchYouTube() {
+                    ctrl.providerVideoList = [];
+                    ctrl.progressBar = true;
+
                     var req = {
                         method: 'GET',
                         url: YOUTUBE_SERARCH_API_URL,
@@ -69,7 +83,23 @@
 
                     $http(req)
                         .then(function (response) {
-                            ctrl.text = response.data;
+                            angular.forEach(response.data.items, function (item) {
+                                var video = {
+                                    provider: "youtube",
+                                    id: item.id.videoId,
+                                    title: item.snippet.title,
+                                    description: item.snippet.description,
+                                    thumbnail: item.snippet.thumbnails.high.url
+                                }
+                                ctrl.providerVideoList.push(video);
+                            });
+                            //ctrl.text = ctrl.providerVideoList;
+                        }, function (error) {
+                            // Error handler
+                            ctrl.text = error.data;
+                        }).finally(function () {
+                            // Always execute this on both error and success
+                            ctrl.progressBar = false;
                         });
                 }
 
@@ -77,32 +107,55 @@
 
                 //Search Vimeo
                 function searchVimeo() {
-                    vimeoToken.then(function (vimeoToken) {
-                        var req = {
-                            method: 'GET',
-                            headers: {
-                                'X-Requested-With': undefined
-                            },
-                            url: VIMEO_SERARCH_API_URL,
-                            params: {
-                                'fields': VIMEO_RETURN_FIELDS,
-                                'per_page': '25',
-                                'query': ctrl.searchVideoTerm,
-                                'access_token': vimeoToken // $cookieStore.get('vimeoToken');
-                            }
-                        };
+                    ctrl.providerVideoList = [];
+                    ctrl.progressBar = true;
 
-                        $http(req)
-                            .then(function (response) {
-                                ctrl.text = response.data;
+                    //vimeoToken.then(function (vimeoToken) {
+                    var req = {
+                        method: 'GET',
+                        headers: {
+                            'X-Requested-With': undefined
+                        },
+                        url: VIMEO_SERARCH_API_URL,
+                        params: {
+                            'fields': VIMEO_RETURN_FIELDS,
+                            'per_page': '25',
+                            'query': ctrl.searchVideoTerm,
+                            'access_token': vimeoToken // $cookieStore.get('vimeoToken');
+                        }
+                    };
+
+                    $http(req)
+                        .then(function (response) {
+                            angular.forEach(response.data.data, function (item) {
+                                var video = {
+                                    provider: "vimeo",
+                                    id: item.uri.split('/videos/')[1],
+                                    title: item.name,
+                                    description: item.description,
+                                    thumbnail: item.pictures.sizes[3].link,
+                                    tags: item.tags
+                                }
+                                ctrl.providerVideoList.push(video);
                             });
-                    });
+                            //ctrl.text = response.data;
+                        }, function (error) {
+                            // Error handler
+                            ctrl.text = error.data.error;
+                        }).finally(function () {
+                            // Always execute this on both error and success
+                            ctrl.progressBar = false;
+                        });
+                    //});
                 }
 
 
 
                 //Search Dailymotion
                 function searchDailymotion() {
+                    ctrl.providerVideoList = [];
+                    ctrl.progressBar = true;
+
                     var req = {
                         method: 'GET',
                         headers: {
@@ -118,7 +171,24 @@
 
                     $http(req)
                         .then(function (response) {
-                            ctrl.text = response.data;
+                            angular.forEach(response.data.list, function (item) {
+                                var video = {
+                                    provider: "dailymotion",
+                                    id: item.id,
+                                    title: item.title,
+                                    description: item.description,
+                                    thumbnail: item.thumbnail_360_url,
+                                    tags: item.tags
+                                }
+                                ctrl.providerVideoList.push(video);
+                            });
+                            //ctrl.text = response.data;
+                        }, function (error) {
+                            // Error handler
+                            ctrl.text = error.data;
+                        }).finally(function () {
+                            // Always execute this on both error and success
+                            ctrl.progressBar = false;
                         });
                 }
             }
