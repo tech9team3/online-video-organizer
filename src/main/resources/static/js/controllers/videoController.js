118c22e5f1e9966918c040dc3c087a4c061cd368
ACurguz <aleksandracurguz@gmail.com>
2017-08-22 15:36:25 +0200

    comment form

--- src/main/resources/static/js/controllers/videoController.js
+++ src/main/resources/static/js/controllers/videoController.js
@@ -2,16 +2,26 @@
     angular.module('app')
             .controller('VideoController', VideoController);
 
-    VideoController.$inject = ['$location', '$http', '$route', 'VideoService', '$routeParams', '$sce'];
+    VideoController.$inject = ['$location', '$http', '$route', 'VideoService', '$routeParams', '$sce', 'CommentService'];
 
-    function VideoController($location, $http, $route, VideoService, $routeParams, $sce) {
+    function VideoController($location, $http, $route, VideoService, $routeParams, $sce, CommentService) {
         var videoCtrl = this;
 
         init();
 
          function init() {
             videoCtrl.embeddedVideoUrl = $sce.trustAsResourceUrl("//www.youtube.com/embed/" + $routeParams.videoId);
-        }      
+        }     
+         
+         function getCommentsForVideo (videoId){
+             CommentService.getCommentsForVideo(videoId).then(handleSuccessComments);
+         }
+         
+         function handleSuccessComments(data, status){
+             videoCtrl.comment = data.data;
+         }
+         
+       
     }
 
 })();
---
+++ src/main/resources/static/js/services/commentService.js
@@ -0,0 +1,70 @@
+(function () {
+    angular.module("app")
+            .service('CommentService', CommentService);
+
+    CommentService.$inject = ['$http', '$q'];
+
+    function CommentService($http, $q) {
+    	
+    	  var commentList = [];
+    	  
+    	  
+	     this.getComment = function (id) {
+	          var def = $q.defer();
+	          var req = {
+	              method: 'GET',
+	              url: "comments/" + id
+	          }
+	          $http(req).success(function (data) {
+	              def.resolve(data);
+	          }).error(function () {
+	              def.reject("Failed");
+	          });
+	          return def.promise;
+	    }
+    	  
+        this.deleteComment = function (id) {
+            var def = $q.defer();
+            var req = {
+                method: 'DELETE',
+                url: "comments/" + id
+            }
+            $http(req).success(function (data) {
+                def.resolve(data);
+            }).error(function () {
+                def.reject("Failed");
+            });
+            return def.promise;
+        }
+        
+        this.getComments = function () {
+            var def = $q.defer();
+            var req = {
+                method: 'GET',
+                url: "comments"
+            }
+            return $http(req).success(function (response) {
+                return commentList = response.data;
+            }).error(function () {
+                return def.reject("Failed to get comment");
+            });
+        } 
+        
+        this.getCommentsForVideo = function (videoId) {
+            var def = $q.defer();
+            var req = {
+                method: 'GET',
+                url: "comments/search/comments/forVideo/" + videoId,
+            }
+            return $http(req).success(function (response) {
+                return commentList = response.data;
+            }).error(function () {
+                return def.reject("Failed to get comment");
+            });
+        } 
+
+       
+
+
+    };
+}());
